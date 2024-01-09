413044a259e687d522fac71febc0732b8b776f50
de019050 <patricstracke@gmail.com>
2024-01-09 15:50:42 +0100

    ok

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,14 +1,23 @@
-
-# Define a function to apply Z-scaling for each charge
+#' Z scaling
+#'
+#' @param data frame
+#' @param charge_col name of col
+#' @param cols_to_scale
+#'
+#' @return A dataframe with additional z scaled coloumns
+#' @export
+#'
+#' @examples jojo
+#'
 scale_per_charge <- function(data, charge_col, cols_to_scale) {
   # Nested function to perform Z-scaling
   z_scale <- function(x) {
-    (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)
+    (x - base::mean(x, na.rm = TRUE)) / stats::sd(x, na.rm = TRUE)
   }
 
   # Apply the Z-scaling for each charge
   data %>%
-    group_by(!!sym(charge_col)) %>%
-    mutate(across(all_of(cols_to_scale), z_scale, .names = "Z_{.col}")) %>%
-    ungroup()
+    dplyr::group_by(!!sym(charge_col)) %>%
+    dplyr::mutate(across(all_of(cols_to_scale), z_scale, .names = "Z_{.col}")) %>%
+    dplyr::ungroup()
 }
